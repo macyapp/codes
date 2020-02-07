8fddb3783c934a4b228be18d585abfc85074b90b
Sidhant Kumar Nayak <macyapp@users.noreply.github.com>
2020-02-07 22:03:32 +0530

    Update square.c

--- c/pattern/square.c
+++ c/pattern/square.c
@@ -1,46 +1,46 @@
 #include<stdio.h>
 void main()
 {
-  int i,j,n=5;
-  for(i=1;i<=n;i++)
-  {
-    for(j=1;j<=i;j++)
+    int i,j,n=5;
+    for(i=1;i<=n;i++)
     {
-      printf("%d ",j);
+        for(j=1;j<=i;j++)
+        {
+            printf("%d ",j);
+        }
+        for(j=n-1;j>=i;j--)
+        {
+            printf("%d ",i);
+        }
+        for(j=n-1;j>=i;j--)
+        {
+            printf("%d ",i);
+        }
+        for(j=i-1;j>=1;j--)
+        {
+            printf("%d ",j);
+        }
+        printf("\n");
     }
-    for(j=n-1;j>=i;j--)
+    n-=1;
+    for(i=n;i>=1;i--)
     {
-      printf("%d ",i);
+        for(j=1;j<=i;j++)
+        {
+            printf("%d ",j);
+        }
+        for(j=n;j>=i;j--)
+        {
+            printf("%d ",i);
+        }
+        for(j=n;j>=i;j--)
+        {
+            printf("%d ",i);
+        }
+        for(j=i-1;j>=1;j--)
+        {
+            printf("%d ",j);
+        }
+        printf("\n");
     }
-    for(j=n-1;j>=i;j--)
-    {
-      printf("%d ",i);
-    }
-    for(j=i-1;j>=1;j--)
-    {
-      printf("%d ",j);
-    }
-    printf("\n");
-  }
-  n-=1;
-  for(i=n;i>=1;i--)
-  {
-    for(j=1;j<=i;j++)
-    {
-      printf("%d ",j);
-    }
-    for(j=n;j>=i;j--)
-    {
-      printf("%d ",i);
-    }
-    for(j=n;j>=i;j--)
-    {
-      printf("%d ",i);
-    }
-    for(j=i-1;j>=1;j--)
-    {
-      printf("%d ",j);
-    }
-    printf("\n");
-  }
 }
