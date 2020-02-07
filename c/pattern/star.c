0f25c701e1c5dcb4467ebeb415052e59c621f041
Sidhant Kumar Nayak <macyapp@users.noreply.github.com>
2020-02-07 21:45:56 +0530

    Update star.c

--- c/pattern/star.c
+++ c/pattern/star.c
@@ -1,38 +1,38 @@
 #include<stdio.h>
 void main()
 {
-  int i,j,n=9;
-  for(i=1;i<=n;i++)
-  {
-    for(j=n-1;j>=i;j--)
+    int i,j,n=9;
+    for(i=1;i<=n;i++)
     {
-      printf("  ");
+        for(j=n-1;j>=i;j--)
+        {
+            printf("  ");
+        }
+        for(j=1;j<=i;j++)
+        {
+            printf("%d ",j);
+        }
+        for(j=i-1;j>=1;j--)
+        {
+            printf("%d ",j);
+        }
+        printf("\n");
     }
-    for(j=1;j<=i;j++)
+    n-=1;
+    for(i=n;i>=1;i--)
     {
-      printf("%d ",j);
+        for(j=n;j>=i;j--)
+        {
+            printf("  ");
+        }
+        for(j=1;j<=i;j++)
+        {
+            printf("%d ",j);
+        }
+        for(j=i-1;j>=1;j--)
+        {
+            printf("%d ",j);
+        }
+        printf("\n");
     }
-    for(j=i-1;j>=1;j--)
-    {
-      printf("%d ",j);
-    }
-    printf("\n");
-  }
-  n-=1;
-  for(i=n;i>=1;i--)
-  {
-    for(j=n;j>=i;j--)
-    {
-      printf("  ");
-    }
-    for(j=1;j<=i;j++)
-    {
-      printf("%d ",j);
-    }
-    for(j=i-1;j>=1;j--)
-    {
-      printf("%d ",j);
-    }
-    printf("\n");
-  }
 }
