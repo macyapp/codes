6563e96862e080ad8a93cefb03b56bbdb9ca1d8d
macyapp <sidhantn99@gmail.com>
2020-02-15 11:31:35 +0530

    Initial Commit

---
+++ c/conditionals/armstrong.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int dig(int x)
+{
+    int p=0;
+    while(x!=0)
+    {
+        x/=10;
+        p++;
+    }
+    return p;
+}
+int main()
+{
+    int num,n,z,m;
+    m=0;
+    printf("Enter a number:\n");
+    scanf("%d",&num);
+    n=num;
+    z=dig(num);
+    while(num!=0)
+    {
+        m+=pow(num%10,z);
+        num/=10;
+    }
+    if(m==n)
+        printf("%d is an Armstrong Number\n",m);
+    else
+        printf("%d is not an Armstrong Number\n",m);
+}
