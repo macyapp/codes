9eb8924af671c1199931f19f3f4a073ba14c1052
macyapp <sidhantn99@gmail.com>
2020-08-14 15:02:42 +0530

    Update

--- python/consec.py
+++ python/consec.py
@@ -2,15 +2,17 @@ num="123779134533234"
 num=[int(x) for x in num]
 print(num)
 sum=list()
-pos=0
-for i in range(pos,len(num)):
-   pr=1
-   for j in range(i,len(num)-1):
-      if ((num[j+1]-num[j])==1):
-         pr*=num[i]
-         pos=j
-      else:
-         pos=j
-         continue
-   sum.append(pr)
+pr=1
+for i in range(len(num)):
+   if i<len(num)-1 and (num[i]+1)==num[i+1]:
+      pr*=num[i]
+   elif i<len(num)-1 and (num[i]+1)!=num[i+1] and (num[i-1]+1)==num[i]:
+      pr*=num[i]
+   elif i==len(num)-1 and (num[i-1]+1)==num[i]:
+      pr*=num[i]
+   else:
+      sum.append(pr)
+      pr=1
+      continue
+
 print(sum)
