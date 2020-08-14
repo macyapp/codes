e58f96f387a4b95c53b84346f90cdfa4618866b7
macyapp <sidhantn99@gmail.com>
2020-08-14 15:24:24 +0530

    Update

--- python/consec.py
+++ python/consec.py
@@ -1,20 +1,29 @@
-num="123779134533234"
+num=input()
+sml=0
 num=[int(x) for x in num]
 print(num)
 sum=list()
 pr=1
+lim=len(num)-1
 for i in range(len(num)):
-   if i<len(num)-1 and (num[i]+1)==num[i+1]:
-      pr*=num[i]
-   elif i<len(num)-1 and (num[i]+1)!=num[i+1] and (num[i-1]+1)==num[i]:
-      pr*=num[i]
-   elif i<len(num)-1 and (num[i]+1)==num[i+1] and (num[i-1]+1)==num[i]:
-      pr*=num[i]
-   elif i==len(num)-1 and (num[i-1]+1)==num[i]:
-      pr*=num[i]
+   if i!=lim:
+      if (num[i]+1)==num[i+1]:
+         pr*=num[i]
+      elif (num[i]+1)!=num[i+1] and (num[i-1]+1)==num[i]:
+         pr*=num[i]
+      else:
+         if pr!=1:
+            sum.append(pr)
+         pr=1
+         continue
    else:
-      sum.append(pr)
-      pr=1
-      continue
-
+      if (num[i-1]+1)==num[i]:
+         pr*=num[i]
+         if pr!=1:
+            sum.append(pr)
 print(sum)
+
+for i in sum:
+   sml+=sum[i]
+   
+print(sml)
