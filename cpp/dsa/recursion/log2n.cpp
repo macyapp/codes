fdb2c281869776cd370ade60a3e9fff54e5653f2
macyapp <sidhantn99@gmail.com>
2021-08-09 19:06:21 +0530

    Update

--- cpp/dsa/recursion/log2n.cpp
+++ cpp/dsa/recursion/log2n.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int fun(int n) {
-    if(n==0)
+    if(n==1)
         return 0;
     return 1+fun(n/2);
 }
@@ -11,6 +11,6 @@ int main() {
     int num;
     cin>>num;
 
-    cout<<"log2n("<<num<<") = "<<fun(num);
+    cout<<"log2n("<<num<<") = "<<fun(num)<<'\n';
     return 0;
 }
