9dcf766344ac88bb05e7b5ddf66d290ce98ba3ee
macyapp <sidhantn99@gmail.com>
2021-08-20 11:46:36 +0530

    ud

---
+++ cpp/dsa/array/remDupseff.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+void remDups(int a[], int *n) {
+    int res=1,i;
+    for(i=1;i<*n;i++) {
+        if(a[res-1]!=a[i]) {
+            a[res]=a[i];
+            res++;
+        }
+    }
+    *n=res;
+}
+
+void printarray(int a[], int n) {
+    for (int i=0;i<n;i++) {
+        cout<<a[i]<<' ';
+    }
+    cout<<'\n';
+}
+
+int main() {
+    int ar[]={10,20,20,20,20,30,30,30};
+    int size=sizeof(ar)/sizeof(ar[0]);
+    cout<<"Array with duplicates:\n";
+    printarray(ar,size);
+    remDups(ar,&size);
+    cout<<"Array without duplicates:\n";
+    printarray(ar,size);
+    return 0;
+}
