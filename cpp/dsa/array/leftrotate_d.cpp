fd99e3ea1df64ffca86f2bbabb7fd42f320627ba
macyapp <sidhantn99@gmail.com>
2021-08-20 21:02:47 +0530

    ud

--- cpp/dsa/array/leftrotate_d.cpp
+++ cpp/dsa/array/leftrotate_d.cpp
@@ -1,13 +1,21 @@
 #include <iostream>
 using namespace std;
 
-void rotatebyone(int a[], int *n) {
+void rotateby_one(int a[], int *n) {
     int temp,i;
     temp=a[0];
     for(i=0;i<*n-1;i++) {
         a[i]=a[i+1];
     }
     a[*n-1]=temp;
+}
+
+void rotateby_d(int a[], int n, int d) {
+    int i;
+    d%=n;
+    for(i=0;i<d;i++) {
+        rotateby_one(a,&n);
+    }
 }
 
 void printarray(int a[], int n) {
@@ -18,11 +26,13 @@ void printarray(int a[], int n) {
 }
 
 int main() {
-    int ar[]={15,7,8,9,0,3};
+    int ar[]={15,7,8,9,0,3},d;
     int size=sizeof(ar)/sizeof(ar[0]);
     cout<<"Array before rotation:\n";
     printarray(ar,size);
-    rotatebyone(ar, &size);
+    cout<<"Enter rotation d value:\n";
+    cin>>d;
+    rotateby_d(ar,size,d);
     cout<<"Array after rotation:\n";
     printarray(ar,size);
     return 0;
