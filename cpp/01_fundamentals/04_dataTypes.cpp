5c1abbb85a3b7ea7ed96b215aa20e504daf30376
Sidhant Kumar Nayak <sidhantn99@gmail.com>
2021-11-01 10:32:33 +0530

    ud

---
+++ cpp/01_fundamentals/04_dataTypes.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+using namespace std ;
+
+int main()
+{   
+    int age = 39; 
+    string name = "Sidhant"; 
+    char gender = 'M';
+    bool isMarried = false; 
+    float height = 163.4; 
+    cout<<name      <<"\n"
+        <<age       <<"\n"
+        <<gender    <<"\n"
+        <<isMarried <<"\n"
+        <<height; 
+    return 0 ;
+}
