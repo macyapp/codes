f75bb09b8aeacb9c37e07efe3473d1f432a32ab7
Sidhant Kumar Nayak <sidhantn99@gmail.com>
2021-11-03 18:16:03 +0530

    ud

--- cpp/01_fundamentals/12_IO.cpp
+++ cpp/01_fundamentals/12_IO.cpp
@@ -3,11 +3,13 @@ using namespace std;
 
 int main()
 {
+
+    // Input
     string name;
     cout << "Please enter your name \n";
     getline(cin,name);
     // cin >> name;    // Wrong approach, try giving full name (it only accepts first word)
-    cout << "Hi " << name << ", \n"
+    cout << "Hi " << name << ", \n"     // 'endl' flushses the stream while '\n' does'nt. So, latter is faster
          << "Welcome to GfG";
     return 0;
 }
