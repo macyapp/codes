38cbc88577780d1513a57bffb2fa8dfc38baf6c8
Sidhant Kumar Nayak <sidhantn99@gmail.com>
2021-11-09 09:39:56 +0530

    ud

--- cpp/dsa/01_introduction/01_intro.cpp
+++ cpp/dsa/01_introduction/01_intro.cpp
@@ -1,7 +1,22 @@
+/*
+Sum of 'n' natural numbers
+O(n^2)
+*/
 #include<iostream>
 using namespace std;
 
+int sum_n(int n) {
+    int i, j, sum=0;
+    for(i=1;i<=n;i++) {
+        for(j=1;j<=i;j++) {
+            sum++;
+        }
+    }
+    return sum;
+}
+
 int main() {
-    int a;
+    int n=10;
+    cout<<"Sum of "<<n<<" natural numbers = "<<sum_n(n)<<'\n';;
     return 0;
 }
