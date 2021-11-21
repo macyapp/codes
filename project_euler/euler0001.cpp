618b4074eca7802182bce05ebbf3bd6ab58fee87
Sidhant Kumar Nayak <sidhantn99@gmail.com>
2021-11-21 23:48:13 +0530

    ud

--- project_euler/euler0001.cpp
+++ project_euler/euler0001.cpp
@@ -2,6 +2,12 @@
 using namespace std;
 
 int main() {
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+    
     int t;
     cin>>t;
     while(t--) {
