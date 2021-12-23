7db567b15259e237df0f97517a2bdde0621142fc
Sidhant Kumar Nayak <sidhantn99@gmail.com>
2021-12-23 19:49:03 +0530

    ud

---
+++ cpp/dsa/test folder/main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    unsigned int x=1;
+    int y=1;
+    cout<<(~x)<<'\n';
+    cout<<(~y)<<'\n';
+    return 0;
+}
